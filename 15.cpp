6bd5e859a6b9fe6a5efd151042ce869630140c77
sosiskins <117184907+sosiskins@users.noreply.github.com>
2022-11-15 18:20:40 +0300

    Add files via upload

---
+++ 15.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+double aMean(double x, double y) {
+	double powerA3(double a) 
+		return a * a * a;
+	}
+	int main() {
+		//Proc1. Описать процедуру PowerA3(A, B), вычисляющую третью степень числа A и возвращающую ее в переменной B (A — входной, B — выходной
+		//параметр; оба параметра являются вещественными).С помощью этой
+		//процедуры найти третьи степени пяти данных чисел.
+	/*	srand(time(nullptr));
+		for (int i = 1; i <= 5; i++) {
+			double x = rand() % 11;
+			double x3 = powerA3(x);
+			cout << " x = " << x << " x^3 = " << x3 << endl;*/
+
+			//Proc2.Описать процедуру PowerA234(A, B, C, D), вычисляющую вторую,
+				//третью и четвертую степень числа A и возвращающую эти степени соответственно в переменных B, C и D(A — входной, B, C, D — выходные параметры; все параметры являются вещественными).С помощью этой
+				//процедуры найти вторую, третью и четвертую степень пяти данных чисел.
+
+			/*srand(time(nullptr));
+			for (int i = 1; i <= 5; i++) {
+			double x = rand() % 11;
+			double x2 = powerA2(x);
+			double x4 = powerA4(x);
+			cout << " x^2 = " << x2 << endl << x^4 << x4 << endl; */
+	}
+	//3
+	/*srand(time(nullptr));
+	for (int i = 1; i <= 10; i++) {
+		double x1 = rand() % 100 * 0.1;
+		double y1 = rand() % 100 * 0, 1;
+		double am = aMean(x1, y1);
+		double am = rand(x1, y1) % 11;
+		cout << x1 << " " << y1 << " am = " << am << " gm = " << gm << endl;
+	}*/
+//4
+	for (int i = 1; i < = 3; i++);
+	{
+		double x;
+		cin << x;
+		double p = trianglep(x);
+		double s = triangles(x);
+		cout << " x = " << x << " p = " << p << " s = " << s << endl;
+
+}
